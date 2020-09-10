d9689076e62bdc9664fb4e3b6edf72402009aa0d
Damian Andrei <69160591+andreidami@users.noreply.github.com>
2020-09-10 18:09:30 +0300

    add homework6medium

---
+++ Homework6.medium/hours.js
@@ -0,0 +1,17 @@
+        if (new Date().getHours() < 12) {
+        console.log("good morning")}
+
+        else if (new Date().getHours() < 18){
+           console.log("good afternoon") }
+
+         else {
+            console.log("Good evening") } 
+            
+            
+                
+         
+            
+
+    
+    
+    
