f443b7b78a0c7509d131daa42d62209c2570aa50
Damian Andrei <69160591+andreidami@users.noreply.github.com>
2020-09-13 07:55:37 +0300

    restore homework6basic

---
+++ Homework6.medium/hours.js
@@ -0,0 +1,9 @@
+if (new Date().getHours() < 12) {
+    console.log("good morning")}
+
+    else if (new Date().getHours() < 18){
+       console.log("good afternoon") }
+
+     else {
+        console.log("Good evening") } 
+        
