d4c4c199bd466c0dd7356af8fc6c3cc97b057e7a
Damian Andrei <69160591+andreidami@users.noreply.github.com>
2020-09-15 13:33:37 +0300

    add Homework8basic

---
+++ Homework8basic/reduce.js
@@ -0,0 +1,8 @@
+var numbers = [1, 2, 3, 4, 5,]
+
+var reverstNumbers = numbers.reduce((number,toatal, index, array) => {
+    return array.reverse(number);
+},[]);
+    
+    console.log(reverstNumbers)
+    
