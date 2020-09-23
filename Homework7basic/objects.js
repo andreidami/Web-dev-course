d86e73ab5e95405c4f848a4bfffd20508fc98b67
Damian Andrei <69160591+andreidami@users.noreply.github.com>
2020-09-23 13:57:08 +0300

    add Homework7basic-Fixed

--- Homework7basic/objects.js
+++ Homework7basic/objects.js
@@ -2,10 +2,15 @@ var animal = {
     type: "squirrel",
     name: "alvin",
     gut: [ "berries", "flowers"],
-    food: "nuts",
+   
 
-    eat: function() {
-        return this.gut + " " + this.food;
+    eat: function(food) {
+
+         this.gut.push(food)}
     }
-}
-     console.log(animal.eat())
+
+     console.log("before:",animal.gut);
+     animal.eat(["nuts", "leaves",]);
+     console.log("full belly:",animal.gut);
+
+
