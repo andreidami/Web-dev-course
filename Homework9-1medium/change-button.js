04ef88fb37e83b1ef1e7545149164871b6fc2d15
Damian Andrei <69160591+andreidami@users.noreply.github.com>
2020-09-29 13:56:49 +0300

    add change-button fixed

--- Homework9-1medium/change-button.js
+++ Homework9-1medium/change-button.js
@@ -6,9 +6,9 @@ const button= document.getElementById("btn");
 var clicks = 0;
 function change() {
   clicks += 1;
-   if(clicks==1)
+   if(clicks===1)
   {message ="Click me again!";}
-   else if(clicks==2)
+   else if(clicks===2)
    {message =  "Thank you, goodbye!";}
   
   else
