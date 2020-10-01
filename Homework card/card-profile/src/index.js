678548c40b08b04d135467b614d4d711058e0cf3
Damian Andrei <69160591+andreidami@users.noreply.github.com>
2020-10-01 23:03:42 +0300

    Homework-card-profile -ready

--- Homework card/card-profile/src/index.js
+++ Homework card/card-profile/src/index.js
@@ -9,6 +9,7 @@ import * as serviceWorker from './serviceWorker';
 const imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQsH_bKAU3UFji6ta58BLKE8wbroM9VpP_pNQ&usqp=CAU"
 const fb = <a href = "https://www.facebook.com/campaign/landing.php?campaign_id=1555708688&extra_1=s%7Cc%7C354150188141%7Ce%7Cfacebook%7C&placement=&creative=354150188141&keyword=facebook&partner_id=googlesem&extra_2=campaignid%3D1555708688%26adgroupid%3D61934849431%26matchtype%3De%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-541132862%26loc_physical_ms%3D1011806%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=CjwKCAjw2dD7BRASEiwAWCtCb4nJPwBCgk8XYSDJSNwXfiIwy5Oe-pohJ_ltG6RoNCkQalNsfgjeTBoCHyQQAvD_BwE">facebook</a>
 const inst = <a href = "https://www.instagram.com/accounts/login/?">Instagram</a>
+const git = <a href = "https://github.com/andreidami">CONTACT</a>
 
 ReactDOM.render(
   <div className = "profile">
@@ -19,11 +20,13 @@ ReactDOM.render(
     <div>
     <button onClick={fb}><a href = "https://www.facebook.com/campaign/landing.php?campaign_id=1555708688&extra_1=s%7Cc%7C354150188141%7Ce%7Cfacebook%7C&placement=&creative=354150188141&keyword=facebook&partner_id=googlesem&extra_2=campaignid%3D1555708688%26adgroupid%3D61934849431%26matchtype%3De%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-541132862%26loc_physical_ms%3D1011806%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=CjwKCAjw2dD7BRASEiwAWCtCb4nJPwBCgk8XYSDJSNwXfiIwy5Oe-pohJ_ltG6RoNCkQalNsfgjeTBoCHyQQAvD_BwE">facebook</a></button>
     <button onCkick={inst}> <a href = "https://www.instagram.com/accounts/login/?">Instagram</a></button>
-    
-    <div className = "contact">CONTACT</div>
+    </div>
+    {/* <div className = "contact"> */}
+      <button className="btn" onClick={git}><a href = "https://github.com/andreidami">CONTACT</a></button>
+    {/* </div> */}
     
 
-    </div>
+   
   </div>,
   
     
